9a988cb967b1e89a80950233d1ac20a8428fbf28
0gwang <ykj0503@gmail.com>
2024-06-20 21:43:17 +0900

    Create 1_page_5.py

---
+++ pages/1_page_5.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.title("참고 문헌")
+st.markdown("""
+- lei 디퓨저 - [https://shop.lei-aroma.com/ko-kr](https://shop.lei-aroma.com/ko-kr)  
+- 열전소자 - [https://gamma0burst.tistory.com/594](https://gamma0burst.tistory.com/594)
+""")
