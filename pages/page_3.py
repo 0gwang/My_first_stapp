a68964bf8bcafb8a77a9cd88d42a459ca50a6960
0gwang <ykj0503@gmail.com>
2024-06-20 21:39:26 +0900

    Create page_3.py

---
+++ pages/page_3.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("기존 제품의 방식과 단점")
+st.image("image/image_page3_2.png")
+st.image("image/image_page3_3.png")
+st.subheader("기존 제품의 방식")
+st.image("image/image_page3_1.png")
+st.markdown("""
+기존 제품 방식 : 향초를 넣어서 열로 모터를 돌리는 방식
+""")
+st.subheader("단점")
+st.markdown("""
+기존 제품 단점 : 향초의 향이 아닌 아로마 오일을 따로 주입하는 단점이 존재함, 30만원이라는 매우 비싼 가격
+""")
