d099433a11398b3299112f0d32d9b92ea2e0edc8
0gwang <ykj0503@gmail.com>
2024-06-20 21:36:51 +0900

    Create 1_page_2.py

---
+++ pages/1_page_2.py
@@ -0,0 +1,12 @@
+import streamlit as st    
+st.title("창업하고 싶은 아이템")
+st.subheader("열전 물질을 이용한 무선 디퓨저")
+st.markdown("""
+열을 동력으로 사용하는 반도체를 이용하여 사용자가 원하는 향을 가진 향초를 기기에 넣고 향초를 점화하면 열이 발생한다. 
+해당 열을 통해서 프로펠러가 돌아가서 디퓨저가 있는 공간에 해당 향이 확산되는 인테리어 소품
+""")
+st.image(["image/image_page2_1.png", 
+          "image/image_page2_2.png", 
+          "image/image_page2_3.png", 
+          "image/image_page2_4.png", 
+          "image/image_page2_5.png"])
